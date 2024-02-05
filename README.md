4e2acdc9fc4bbc31da19ced5fce45b9b57a512d0
trickaugusto <trickaugusto1206@gmail.com>
2024-02-05 00:24:07 -0300

    feat: add routes logic

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Golang Studies =D
+
+Packages:
+- When you create a package, you can use it in another package by importing it.
+- The package name is the same as the last element of the import path.
+- To export a function, make the first letter of the name uppercase.
+- All the functions in a package are visible to all the files in the same package.
--- router/router.go
+++ router/router.go
@@ -4,16 +4,12 @@ import "github.com/gin-gonic/gin"
 
 // When the first letter of a function's name is capitalized, it means that it is public and exported
 func Initialize() {
-	// Initialize the default Gin router
+	// Initiallize Router
 	// r is a ephemeral variable
 	router := gin.Default()
 
-	// Define a route handler for the GET /ping route
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// Initialize routes
+	initializeRoutes(router)
 
 	// Run the server
 	router.Run(":8080")
